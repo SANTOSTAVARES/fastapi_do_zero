2c541c3a281135db03622dec2abd47e7c4316975
Gabriel Tavares <gsantostavares@gmail.com>
2024-07-14 12:41:02 -0300

    início da implementação do UserDB

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from http import HTTPStatus
+from fast_zero.schemas import UserPublic, UserSchema
 
 app = FastAPI()
 
 
-@app.get('/')
+#@app.get("/", status_code=HTTPStatus.OK)
+@app.get('/', status_code=200)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    ...
