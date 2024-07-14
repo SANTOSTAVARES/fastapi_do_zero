3b4b275e3883f35ef75a156e81011f1737fd90a2
Gabriel Tavares <gsantostavares@gmail.com>
2024-07-14 12:42:20 -0300

    a classe UserDB, deve ser finalizada e usada provisoriamente. -Início da construção dela

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -12,4 +12,8 @@ def read_root():
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    ...
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)  
+
+    database.append(user_with_id)
+
+    return user_with_id
