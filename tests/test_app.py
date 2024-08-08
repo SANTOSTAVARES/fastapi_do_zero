656650cf5ea144e041a3785f3a03c4605507286c
Gabriel Tavares <gsantostavares@gmail.com>
2024-08-08 12:13:28 -0300

    alteração do teste de get_token para o novo arquivo test_auth.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -66,13 +66,4 @@ def test_delete_user(client, user, token):
     assert response.json() == {'message': 'User deleted'}
 
 
-def test_get_token(client, user):
-    response = client.post(
-        '/auth/token',
-        data={'username': user.email, 'password': user.clean_password},
-    )
-    token = response.json()
 
-    assert response.status_code == HTTPStatus.OK
-    assert 'access_token' in token
-    assert 'token_type' in token
