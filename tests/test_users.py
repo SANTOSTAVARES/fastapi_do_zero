9fbd5e7958480267ec789bc63585c732a6571af9
Gabriel Tavares <gsantostavares@gmail.com>
2024-08-14 23:56:27 -0300

    criação do test_users.py e test_update_user_with_wrong_user

---
+++ tests/test_users.py
@@ -0,0 +1,12 @@
+def test_update_user_with_wrong_user(client, user, token):
+    response = client.put(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.json() == {'detail': 'Not enough permissions'}
